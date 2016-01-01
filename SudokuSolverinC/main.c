67516c749290368b258909019cfcf57ad7331f88
Janus <januslinhc@hotmail.com>
2016-01-01 16:26:48 +0800

    Initial Commit

---
+++ SudokuSolverinC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SudokuSolverinC
+//
+//  Created by Janus on 1/1/2016.
+//  Copyright © 2016年 Janus. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
